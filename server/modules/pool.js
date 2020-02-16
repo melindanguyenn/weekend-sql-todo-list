671a85d5a174ce988f94682dfd3dba138f02c4d7
Melinda Nguyen <melindanguyenn@gmail.com>
2020-02-15 20:42:43 -0600

    html body

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,2 +1,24 @@
 //db connection here
 //module.exports = ;
+
+const pg = require('pg');
+
+const config = {
+    database: 'weekend-to-do-app',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000
+};
+
+const pool = new pg.Pool(config);
+
+pool.on("connect", () => {
+    console.log("connected to postgres");
+});
+
+pool.on("error", (err) => {
+    console.log("error connecting to postgres", err);
+});
+
+module.exports = pool;
