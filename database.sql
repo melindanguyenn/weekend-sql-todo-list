04679bf34c9ccaab31e63ce4cda7ffe3132aef3c
Melinda Nguyen <melindanguyenn@gmail.com>
2020-02-17 09:15:06 -0600

    remove manual date insertion in DB

--- database.sql
+++ database.sql
@@ -6,14 +6,14 @@ CREATE TABLE "tasks"(
     "status" VARCHAR (80) DEFAULT 'INCOMPLETE'
 );
 
-INSERT INTO "tasks"("date", "task", "notes") VALUES(current_date, 'Schedule doctors appointment', 'Papercut');
-INSERT INTO "tasks"("date", "task", "notes") VALUES(current_date, 'Cancel cater', 'Cancel by Moday!!! Caterer: McDonalds');
-INSERT INTO "tasks"("date", "task", "notes") VALUES(current_date, 'Clean room', 'Laundry and vacuum');
-INSERT INTO "tasks"("date", "task", "notes") VALUES(current_date, 'Dinner date', 'Dinner at 6PM Monday with friends, BRING WINE');
-INSERT INTO "tasks"("date", "task", "notes") VALUES(current_date, 'Pick up family', 'Landing at MSP Wednesday at 8PM');
-INSERT INTO "tasks"("date", "task", "notes") VALUES(current_date, 'Buy groceries', 'Apples, bananas, oranges, lettuce, tomato, eggs, ham');
-INSERT INTO "tasks"("date", "task", "notes") VALUES(current_date, 'Bring dog to groomers', 'PetSmart at 12PM with XYZ');
-INSERT INTO "tasks"("date", "task", "notes") VALUES(current_date, 'Garden', 'Replace fertilizer, add new flowers, clean fish pond, refill bird feeder,  trim trees');
-INSERT INTO "tasks"("date", "task", "notes") VALUES(current_date, 'Food tasting', 'Food tasting at Costco with friends');
-INSERT INTO "tasks"("date", "task", "notes") VALUES(current_date, 'Call Dad', 'Check in, get updates');
-INSERT INTO "tasks"("date", "task", "notes") VALUES(current_date, 'Make cake', 'For XYZs birthday');
+INSERT INTO "tasks"("task", "notes") VALUES('Schedule doctors appointment', 'Papercut');
+INSERT INTO "tasks"("task", "notes") VALUES('Cancel cater', 'Cancel by Moday!!! Caterer: McDonalds');
+INSERT INTO "tasks"("task", "notes") VALUES('Clean room', 'Laundry and vacuum');
+INSERT INTO "tasks"("task", "notes") VALUES('Dinner date', 'Dinner at 6PM Monday with friends, BRING WINE');
+INSERT INTO "tasks"("task", "notes") VALUES('Pick up family', 'Landing at MSP Wednesday at 8PM');
+INSERT INTO "tasks"("task", "notes") VALUES('Buy groceries', 'Apples, bananas, oranges, lettuce, tomato, eggs, ham');
+INSERT INTO "tasks"("task", "notes") VALUES('Bring dog to groomers', 'PetSmart at 12PM with XYZ');
+INSERT INTO "tasks"("task", "notes") VALUES('Garden', 'Replace fertilizer, add new flowers, clean fish pond, refill bird feeder,  trim trees');
+INSERT INTO "tasks"("task", "notes") VALUES('Food tasting', 'Food tasting at Costco with friends');
+INSERT INTO "tasks"("task", "notes") VALUES('Call Dad', 'Check in, get updates');
+INSERT INTO "tasks"("task", "notes") VALUES('Make cake', 'For XYZs birthday');
